90df0f92719cf7097ec5434f26d544622ab87c8d
kalakal <sayedfaizanqadri@gmail.com>
2022-11-08 01:32:27 +0530

    adressbook uc2 added

--- Program.cs
+++ Program.cs
@@ -3,66 +3,176 @@
 namespace Adressbook_Assignments
 {
 
-    class ContactPersons
-    {
-        private String firstName;
 
-        private String address;
-        private String lastName;
-        private String city;
-        private String email;
-        private String state;
-        private String zip;
-        private String phoneNumber;
-
-        public ContactPersons(String firstName, String lastName, String address, String city, String state, String zip, String phoneNumber, String email)
-        {
-            this.firstName = firstName;
-            this.lastName = lastName;
-            this.address = address;
-            this.zip = zip;
-            this.city = city;
-            this.state = state;
-            this.phoneNumber = phoneNumber;
-            this.email = email;
-        }
-        //Override
-        public String toString()
-        {
-            return "In AddressBook" +
-            "\n-------" +
-               "\n FirstName=" + firstName +
-               "\n LastName=" + lastName +
-               "\n Address=" + address +
-               "\n City=" + city +
-               "\n State=" + state +
-               "\n Zip=" + zip +
-               "\n PhoneNumber=" + phoneNumber +
-               "\n Email=" + email;
-        }
-    }
-    public class AddressBookSystemMain
+    class Program
     {
 
-        //Default Constructor
 
-        public AddressBookSystemMain()
+        static string userStringEntry;
+        static int userIntEntry;
+        static Contact contact1 = new Contact();
+
+        static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Address Book Program !!!");
+            Console.WriteLine("Address Book.");
+            MainMenu();
+
+            Console.ReadKey();
         }
-        public static void Main(String[] args)
+
+
+        static void MainMenu()
         {
-            //Initialize Object
-            AddressBookSystemMain obj = new AddressBookSystemMain();
 
-            //Calling Encapsulated Class object
+            Console.WriteLine("\n1 - Add Contact.");
+            Console.WriteLine("2 - Remove Contact");
+            Console.WriteLine("3 - List Contacts.");
+            Console.WriteLine("4 - Quit.");
 
-            ContactPersons addressBook = new ContactPersons("SayedFaizan", "Faizan", "rajouri", "Jammu",
+            userStringEntry = Console.ReadLine();
+            userIntEntry = System.Int32.Parse(userStringEntry);
 
-         "JAMMU AND KASHMIR", "185131", "9906276508", "sayedfaizanqadri@gmail.com");
+            switch (userIntEntry)
+            {
+                case 1:
+                    AddContact();
+                    break;
 
-            Console.WriteLine("\n" + addressBook.toString());
+                case 2:
+                    RemoveContact();
+                    break;
 
+                case 3:
+                    ListContacts();
+                    break;
+
+                case 4:
+                    Environment.Exit(0);
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid choice. Please enter sufficient data.");
+                    break;
+            }
         }
+
+        static void AddContact()
+        {
+            do
+            {
+                Console.WriteLine("\nEnter contacts first name.");
+                contact1.FirstName = Console.ReadLine();
+
+                Console.WriteLine("Enter contacts last name.");
+                contact1.LastName = Console.ReadLine();
+
+                Console.WriteLine("Enter contacts street address.");
+                contact1.StreetAddress = Console.ReadLine();
+
+                Console.WriteLine("Enter contacts town/city.");
+                contact1.TownCity = Console.ReadLine();
+
+                Console.WriteLine("Enter the contacts county.");
+                contact1.County = Console.ReadLine();
+
+                Console.WriteLine("Enter the contacts country.");
+                contact1.Country = Console.ReadLine();
+
+                Console.WriteLine("Enter the contacts phone number.");
+                userStringEntry = Console.ReadLine();
+                contact1.PhoneNumber = long.Parse(userStringEntry);
+
+
+                Console.WriteLine("\nIs the following information correct?");
+
+                Console.WriteLine("\nContacts first name: " + contact1.FirstName);
+                Console.WriteLine("Contacts last name: " + contact1.LastName);
+                Console.WriteLine("Contacts street address: " + contact1.StreetAddress);
+                Console.WriteLine("Contacts town/city: " + contact1.TownCity);
+                Console.WriteLine("Contacts county: " + contact1.County);
+                Console.WriteLine("Contacts country: " + contact1.Country);
+                Console.WriteLine("Contacts phone number: " + contact1.PhoneNumber);
+
+
+
+                Console.WriteLine("\n1 - Yes. 2 - Retry.");
+
+                userStringEntry = Console.ReadLine();
+                userIntEntry = System.Int32.Parse(userStringEntry);
+            }
+            while (userIntEntry == 2);
+
+            if (userIntEntry == 1)
+            {
+                MainMenu();
+            }
+        }
+
+        static void RemoveContact()
+        {
+            Console.WriteLine("\n1 - Remove all contacts. 2 - Main Menu.");
+            userStringEntry = Console.ReadLine();
+            userIntEntry = System.Int32.Parse(userStringEntry);
+
+            if (userIntEntry == 1)
+            {
+                contact1.FirstName = "";
+                contact1.LastName = "";
+                contact1.StreetAddress = "";
+                contact1.TownCity = "";
+                contact1.County = "";
+                contact1.Country = "";
+                contact1.PhoneNumber = 0;
+
+                Console.WriteLine("\nAll contacts have been removed.");
+
+                Console.WriteLine("\n1 - Main Menu");
+
+                userStringEntry = Console.ReadLine();
+                userIntEntry = System.Int32.Parse(userStringEntry);
+
+                switch (userIntEntry)
+                {
+                    case 1:
+                        MainMenu();
+                        break;
+
+                    default:
+                        Console.WriteLine("Please enter sufficient data.");
+                        break;
+
+                }
+
+            }
+        }
+
+        static void ListContacts()
+        {
+            Console.WriteLine("\n\nContact 1.");
+
+            Console.WriteLine("\n" + contact1.FirstName + "" + contact1.LastName);
+            Console.WriteLine(contact1.StreetAddress + " ");
+            Console.WriteLine(contact1.TownCity + " ");
+            Console.WriteLine(contact1.County + " ");
+            Console.WriteLine(contact1.Country + " ");
+            Console.WriteLine(contact1.PhoneNumber + "");
+
+            Console.WriteLine("\n1 - Main Menu");
+
+            userStringEntry = Console.ReadLine();
+            userIntEntry = System.Int32.Parse(userStringEntry);
+
+            switch (userIntEntry)
+            {
+                case 1:
+                    MainMenu();
+                    break;
+
+                default:
+                    Console.WriteLine("Please enter sufficient data.");
+                    break;
+            }
+        }
+
     }
 }
